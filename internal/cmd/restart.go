ac13d42411e82b0b1c1bbb3a2c2d12af80f71e8d
RenterRus <artur.dawydov2013@yandex.ru>
2024-11-24 05:21:15 +0300

    beauty

--- internal/cmd/restart.go
+++ internal/cmd/restart.go
@@ -3,13 +3,12 @@ package cmd
 import (
 	"fmt"
 	"os/exec"
-	"strconv"
 )
 
 func (c *CMD) Restart() (string, bool) {
-	if err := exec.Command("/bin/sh", "-c", "sudo shutdown -r +"+strconv.Itoa(c.ttp)).Run(); err != nil {
+	if err := exec.Command("/bin/sh", "-c", "sudo reboot now").Run(); err != nil {
 		return fmt.Sprintf("reboot error: %s", err.Error()), false
 	}
 
-	return "Restart is planing after " + strconv.Itoa(c.ttp) + " minutes. But the bot shutdown right now", true
+	return "Restart is run", true
 }
