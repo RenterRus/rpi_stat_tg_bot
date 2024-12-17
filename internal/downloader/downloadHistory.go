96c5192baac9f7e2f23eda15f27ab3eaafb8e6c0
RenterRus <artur.dawydov2013@yandex.ru>
2024-12-17 04:54:51 +0300

    refactoring

--- internal/downloader/downloadHistory.go
+++ internal/downloader/downloadHistory.go
@@ -16,10 +16,11 @@ func (d *DLP) getHistory(mode string) (string, int) {
 		res += "\n" + strings.ToUpper(mode) + "\n"
 		ch = len(links)
 		for k, v := range links {
-			if v.Name == "" {
-				v.Name = "We\\'ll find out soon"
+			if mode == db.StatusDONE {
+				res += fmt.Sprintf("%d. %s [%s]\n", (k + 1), v.Name, v.Link)
+			} else {
+				res += fmt.Sprintf("%d. Coming soon [%s]\n", (k + 1), v.Link)
 			}
-			res += fmt.Sprintf("%d. %s [%s]\n", (k + 1), v.Name, v.Link)
 		}
 	}
 
