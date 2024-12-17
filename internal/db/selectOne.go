4e53c582896f552e46c2dd2f9f3ed41489dfa05a
RenterRus <artur.dawydov2013@yandex.ru>
2024-12-17 05:06:16 +0300

    refactoring

--- internal/db/selectOne.go
+++ internal/db/selectOne.go
@@ -11,17 +11,17 @@ func (m *manager) SelectOne() (string, error) {
 	}
 	defer m.close()
 
-	rows, err := m.db.Query("select link from links where status = $1 order by RANDOM() limit 1", StatusNEW)
-	defer func() {
-		rows.Close()
-	}()
+	rows, err := m.db.Query("select link, name from links where status = $1 order by RANDOM() limit 1", StatusNEW)
 	if err != nil {
 		return "", fmt.Errorf("db.SelectOne(query): %w", err)
 	}
+	defer func() {
+		rows.Close()
+	}()
 
 	rows.Next()
 	p := links{}
-	err = rows.Scan(&p.Link)
+	err = rows.Scan(&p.Link, &p.Name)
 	if err != nil {
 		return "", fmt.Errorf("db.SelectOne(Scan): %w", err)
 	}
