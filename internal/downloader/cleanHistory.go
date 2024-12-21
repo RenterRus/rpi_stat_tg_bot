1d9a0e1065c862b8793426471ee79af959785b83
RenterRus <artur.dawydov2013@yandex.ru>
2024-12-21 21:17:27 +0300

    switch limit workers into config and change keyborad

--- internal/downloader/cleanHistory.go
+++ internal/downloader/cleanHistory.go
@@ -7,5 +7,8 @@ func (d *DLP) CleanHistory() string {
 		return fmt.Errorf("CleanHistory: %w", err).Error()
 	}
 
+	d.totalComplete.Store(0)
+	d.totalRetry.Store(0)
+
 	return fmt.Sprintf("The history has been cleared\n\n%s\n", d.DownloadHistory())
 }
